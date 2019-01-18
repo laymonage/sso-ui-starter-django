2690d893becd736d4f4e968b3b849935d3c8adda
sage <laymonage@gmail.com>
2019-01-18 14:29:13 +0700

    Add tests

--- sso_ui/tests.py
+++ sso_ui/tests.py
@@ -1,3 +1,109 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.urls import reverse
 from django.test import TestCase
+from django_cas_ng.signals import cas_user_authenticated
 
-# Create your tests here.
+from .models import ORG_CODE, Profile
+
+import json
+
+
+class SSOUITest(TestCase):
+    ATTRIBUTES = {
+        "nama": "Ice Bear",
+        "peran_user": "mahasiswa",
+        "npm": "1706123123",
+        "kd_org": "01.00.12.01"
+    }
+
+    def setUp(self):
+        self.user = User.objects.create_superuser(
+            username='username', password='password', email='username@test.com'
+        )
+
+    def test_home_url_exists(self):
+        response = self.client.get(reverse('sso_ui:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_url_exists(self):
+        response = self.client.get(reverse('sso_ui:login'))
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.url.startswith(settings.CAS_SERVER_URL))
+
+    def test_logout_url_exists(self):
+        response = self.client.get(reverse('sso_ui:logout'))
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.url.startswith(settings.CAS_SERVER_URL))
+
+    def test_profile_url_redirect_unauthenticated(self):
+        response = self.client.get(reverse('sso_ui:profile'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_profile_can_save_attributes(self):
+        cas_user_authenticated.send(
+            sender=self,
+            user=self.user,
+            created=False,
+            attributes=SSOUITest.ATTRIBUTES
+        )
+        self.assertJSONEqual(
+            json.dumps({
+                "nama": self.user.get_full_name(),
+                "peran_user": self.user.profile.role,
+                "npm": self.user.profile.npm,
+                "kd_org": self.user.profile.org_code
+            }),
+            SSOUITest.ATTRIBUTES
+        )
+        self.assertJSONEqual(
+            json.dumps({
+                "faculty": self.user.profile.faculty,
+                "study_program": self.user.profile.study_program,
+                "educational_program": self.user.profile.educational_program
+            }),
+            ORG_CODE['id'][SSOUITest.ATTRIBUTES['kd_org']]
+        )
+        self.assertEqual(self.user.email, f"{self.user.username}@ui.ac.id")
+        self.assertEqual(self.user.first_name, "Ice")
+        self.assertEqual(self.user.last_name, "Bear")
+
+    def test_profile_str(self):
+        self.assertEqual(str(self.user.profile), self.user.username)
+
+    def test_profile_url_show_data_authenticated(self):
+        cas_user_authenticated.send(
+            sender=self,
+            user=self.user,
+            created=False,
+            attributes=SSOUITest.ATTRIBUTES
+        )
+        self.client.login(username='username', password='password')
+        response = self.client.get(reverse('sso_ui:profile'))
+        self.assertEqual(response.status_code, 200)
+        content = response.content.decode('utf-8')
+        attributes = [
+            self.user.get_full_name(),
+            self.user.username,
+            self.user.email,
+            self.user.profile.org_code,
+            self.user.profile.role,
+            self.user.profile.npm,
+            self.user.profile.faculty,
+            self.user.profile.study_program,
+            self.user.profile.educational_program
+        ]
+        for attr in attributes:
+            self.assertIn(attr, content)
+
+    def test_admin_cant_change_profile(self):
+        self.client.login(username='username', password='password')
+        response = self.client.get(
+            reverse(
+                'admin:sso_ui_profile_change',
+                kwargs={'object_id': self.user.profile.id}
+            )
+        )
+        content = response.content.decode('utf-8')
+        self.assertNotIn('<input type="text"', content)
+        self.assertIn('<div class="readonly">', content)
